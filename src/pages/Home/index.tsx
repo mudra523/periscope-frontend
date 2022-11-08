0923aa7a34f6658d3156c3d478295de7eb518347
mudra523 <mudrakoradia1999@gmail.com>
2022-11-08 20:03:54 +0530

    Authentication Setup With Redux

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../layout/index";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return (
+    <Layout>
+      <>{console.log("Home")}</>
+      Home
+    </Layout>
+  );
+};
+
+export default Home;
---
+++ src/pages/PageNotFound/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from "../../layout/index";
+
+type Props = {}
+
+const PageNotFound = (props: Props) => {
+  return (
+    <Layout>
+      <>{console.log("Home")}</>
+      PageNotFound
+    </Layout>
+  )
+}
+
+export default PageNotFound
